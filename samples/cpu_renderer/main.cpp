312ea6d898f8d474279c1a2a81d4b890c4b890a8
shocker-0x15 <shocker.0x15@gmail.com>
2024-07-23 00:54:47 +0900

    update the spec. added a sample program.

--- samples/cpu_renderer/main.cpp
+++ samples/cpu_renderer/main.cpp
@@ -1,6 +1,8 @@
 ﻿#include <cstdio>
 #include <cstdint>
 #include <cstdlib>
+#include <cmath>
+#include <numbers>
 #include <vector>
 #include <string_view>
 #include <chrono>
@@ -14,28 +16,21 @@ int32_t main(int32_t argc, const char* argv[]) {
     using clock = std::chrono::high_resolution_clock;
     const clock::time_point appStartTp = clock::now();
 
-    uint32_t startFrameIndex = 0;
-    uint32_t endFrameIndex = 0;
+    uint32_t timeLimit = UINT32_MAX;
     for (int argIdx = 1; argIdx < argc; ++argIdx) {
         std::string_view arg = argv[argIdx];
-        if (arg == "--frame-range") {
-            if (argIdx + 2 >= argc) {
-                printf("--frame-range requires a frame number pair to start and end.\n");
+        if (arg == "--time-limit") {
+            if (argIdx + 1 >= argc) {
+                printf("missing value.\n");
                 return -1;
             }
-            startFrameIndex = static_cast<uint32_t>(atoi(argv[argIdx + 1]));
-            endFrameIndex = static_cast<uint32_t>(atoi(argv[argIdx + 2]));
-            argIdx += 2;
+            timeLimit = static_cast<uint32_t>(atoi(argv[argIdx + 1]));
+            argIdx += 1;
         }
         else {
             printf("Unknown argument %s.\n", argv[argIdx]);
             return -1;
         }
-    }
-
-    if (endFrameIndex < startFrameIndex) {
-        printf("Invalid frame range.\n");
-        return -1;
     }
 
 
@@ -53,28 +48,30 @@ int32_t main(int32_t argc, const char* argv[]) {
     constexpr uint32_t height = 256;
     std::vector<RGBA> pixels(height * width);
 
-    for (uint32_t frameIndex = startFrameIndex; frameIndex <= endFrameIndex; ++frameIndex) {
+    for (uint32_t frameIndex = 0; frameIndex < 600; ++frameIndex) {
         const clock::time_point frameStartTp = clock::now();
         printf("Frame %u ... ", frameIndex);
 
         // 高度なレンダリング...
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
         for (int y = 0; y < height; ++y) {
             for (int x = 0; x < width; ++x) {
                 RGBA v;
                 v.r = x;
                 v.g = y;
-                v.b = frameIndex;
+                v.b = 127 + 127 * std::sinf(2 * std::numbers::pi_v<float> * (frameIndex % 60) / 60.0f);
                 v.a = 255;
                 const int32_t idx = y * width + x;
                 pixels[idx] = v;
             }
         }
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
 
         // 起動からの時刻とフレーム時間を計算。
         const clock::time_point now = clock::now();
         const clock::duration frameTime = now - frameStartTp;
         const clock::duration totalTime = now - appStartTp;
+        if (std::chrono::duration_cast<std::chrono::milliseconds>(totalTime).count() > 1e+3f * timeLimit)
+            break;
         printf(
             "Done: %.3f [ms] (total: %.3f [s])\n",
             std::chrono::duration_cast<std::chrono::microseconds>(frameTime).count() * 1e-3f,
---
+++ yourname_cpu/run.ps1
@@ -0,0 +1,1 @@
+.\cpu_renderer --time-limit 256
